42182144ab55c3fce08893cf5b9fbf5171c869b7
IyedBecheikh <iyed.becheikh@itbs.tn>
2023-05-30 17:36:28 +0100

    faker implementation

--- backend/index.js
+++ backend/index.js
@@ -52,9 +52,9 @@ app.use((req, res, next) => {
 });
 
 
-
 // Start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
